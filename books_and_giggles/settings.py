50d2013f27902fbe99b5f995d0a4aa23a055ef49
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-10-10 11:58:34 +0000

    Readied for deployment

--- books_and_giggles/settings.py
+++ books_and_giggles/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(l*!+$l4lbd1tccph^kq1i=n6jv5*kg=dsp97kzr^=u^vs0j@)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-mienjung97-booksandgigg-gih9t2qszhe.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = [
+    '8000-mienjung97-booksandgigg-gih9t2qszhe.ws.codeinstitute-ide.net',
+    '.herokuapp.com,'
+    ]
 
 
 # Application definition
