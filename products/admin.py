90992e2e02cbc762d46be77ddca9ad68df6101fa
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-10-12 14:36:50 +0000

    Customized admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,54 @@
 from django.contrib import admin
-from. models import Product, Category, Author, Cover
+from .models import Product, Category, Author, Cover
+
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Author)
-admin.site.register(Cover)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'get_categories',
+        'author',
+        'cover',
+        'price',
+        'image',
+    )
+
+    # since one book can have more than one category, this is the helper function:
+    # It sorts through the current product, checks for categories and joins them
+    # with a comma. The last line just changes the display name in the Admin panel
+    def get_categories(self, product):
+        return ', '.join(
+            [category.name for category in product.category.all()]
+        )
+
+    get_categories.short_description = 'Categories'
+
+    ordering = ('author',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class CoverAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Cover, CoverAdmin)
