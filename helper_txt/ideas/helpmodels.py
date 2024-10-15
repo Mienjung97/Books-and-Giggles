cd95faa1f8e873b100ef7f4dc09478e9f7256e5e
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-10-15 17:34:53 +0000

    Edited products txt, prepped for renewed models py file

--- helper_txt/ideas/helpmodels.py
+++ helper_txt/ideas/helpmodels.py
@@ -18,21 +18,7 @@ class Category(models.Model):
 
 class Author(models.Model):
     name = models.CharField(max_length=254)
-
-    def __str__(self):
-        return self.name
-
-
-class Cover(models.Model):
-    name = models.CharField(max_length=254)
-
-    def __str__(self):
-        return self.name
-        
-    
-class Years(models.Model):
-    name = models.CharField(max_length=254)
-
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -41,16 +27,25 @@ class Years(models.Model):
         return self.friendly_name
 
 
+class Cover(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     category = models.ManyToManyField('Category', related_name='products')
     author = models.ForeignKey('Author', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    description2 = models.TextField(null=True, blank=True)
+    year_published = models.PositiveIntegerField(null=True, blank=True)
+    publisher = models.CharField(max_length=254, null=True, blank=True)
+    page_count = models.PositiveIntegerField(null=True, blank=True)
     cover = models.ForeignKey('Cover', null=True, blank=True, on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    ISBN = models.CharField(max_length=13, null=True, blank=True)
-    book_url = models.URLField(max_length=1024, null=True, blank=True)
+    isbn = models.CharField(max_length=13, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
