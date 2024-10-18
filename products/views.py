f369c9dab24baab9b86d3804158314bdc5c66e77
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-10-18 12:23:52 +0000

    Implemented search queries, minor changes in products and nav templates and folder hierarchy

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Author, Category
 
 # Create your views here.
+
 
 def all_products(request):
     """
@@ -9,12 +12,38 @@ def all_products(request):
     """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!"
+                )
+                return redirect(reverse('products'))
+
+            queries = (
+                Q(name__icontains=query)
+                | Q(description__icontains=query)
+                | Q(description2__icontains=query)
+                | Q(extra_info__icontains=query)
+                | Q(publisher__icontains=query)
+                | Q(isbn__icontains=query)
+                | Q(author__name__icontains=query)
+                | Q(author__friendly_name__icontains=query)
+                | Q(category__name__icontains=query)
+                | Q(category__friendly_name__icontains=query)
+            )
+            products = products.filter(queries).distinct()
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
+
 
 def product_detail(request, product_id):
     """
