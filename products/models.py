2601ee6db186cc0657293b2cdd3bbbc23e94e7a2
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-10-27 14:08:15 +0000

    Added description and img for authors, bugfix in product detail and checkout success templates

--- products/models.py
+++ products/models.py
@@ -19,6 +19,8 @@ class Category(models.Model):
 class Author(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
