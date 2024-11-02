a151f89b9ee313d01a06b095279b10bc02de180e
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-11-02 17:52:46 +0000

    Styled FAQ page

--- faq/models.py
+++ faq/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Faq(models.Model):
 
     question = models.CharField(max_length=254)
-    answer = models.CharField(max_length=500)
+    answer = models.TextField(max_length=2000, blank=False)
 
     def __str__(self):
         return self.question
