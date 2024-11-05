6a0ef180c39234e6f11879844150c9a2fdee7e77
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-11-05 17:21:49 +0000

    Added product delete confirmation page

--- products/urls.py
+++ products/urls.py
@@ -6,15 +6,16 @@ urlpatterns = [
     path('management/', views.management, name='management'),
     path('authors/', views.all_authors, name='authors'),
     path('add_author/', views.add_author, name='add_author'),
-    path('edit/<int:author_id>/', views.edit_author, name='edit_author'),
+    path('edit_author/<int:author_id>/', views.edit_author, name='edit_author'),
     path(
         'delete/<int:author_id>/', views.delete_author, name='delete_author'
     ),
     path('add_category/', views.add_category, name='add_category'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
-    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-    path(
-        'delete/<int:product_id>/', views.delete_product, name='delete_product'
-    ),
+    path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
+    # path(
+    #     'delete/<int:product_id>/', views.delete_product, name='delete_product'
+    # ),
 ]
--- products/views.py
+++ products/views.py
@@ -230,9 +230,18 @@ def delete_product(request, product_id):
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
-    product.delete()
-    messages.success(request, 'Product deleted!')
-    return redirect(reverse('products'))
+
+    if request.method == "POST":
+        product.delete()
+        messages.success(request, 'Product deleted!')
+        return redirect("home")
+    
+    template ='products/delete_product.html'
+    context = {
+        'product': product
+    }
+
+    return render(request, template, context)
 
 
 # Categories
