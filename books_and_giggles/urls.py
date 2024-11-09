de9bcff83e9044e7f316cc2d06b94de8d77d1ba3
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-11-09 10:59:17 +0000

    Added error templates, views and urls, changed home page layout

--- books_and_giggles/urls.py
+++ books_and_giggles/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import handler404, handler500
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
